f2f91806ebc85bd78f335022833cc41d581b1361
Gabriel Silva <gabrieljosebilla@gmail.com>
2026-08-14 10:23:12 -0300

    PDI v0.7

---
+++ app.js
@@ -0,0 +1,15 @@
+const bombas = [
+    {
+        serial: "71005423152",
+        id: "852a22s1d54841d1a51245ad152",
+        modelo: "V3340F362G-2",
+        descricao: "Bomba Perkins"
+    }
+];
+
+const campoSerial = document.getElementById("serial");
+const botaoFinalizar = document.getElementById("btn-finalizar");
+
+botaoFinalizar.addEventListener("click", function () {
+    console.log("Botão Finalizar clicado!");
+});
